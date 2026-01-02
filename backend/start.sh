67f146a20dcc6fca8f2bb73ef6c3ecb3eee251bd
SukJin Kim <rkathrwks@gmail.com>
2026-01-02 20:35:45 +0900

    feat: 환경설정 파일 경로 수정

--- backend/start.sh
+++ backend/start.sh
@@ -3,4 +3,4 @@
 export CORS_ALLOW_ORIGIN="http://localhost:5173"
 
 PORT="${PORT:-8000}"
-uvicorn app.main:app --env-file .env --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
+uvicorn app.main:app --env-file ../.env --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
